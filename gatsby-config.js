ce27a2bcc996efb65bdfca2684ff924e653da001
Gorazd <492973@mail.muni.cz>
2022-01-18 18:07:29 +0100

    Fix source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,25 @@
 module.exports = {
   plugins: [
     // Tailwind
-    'gatsby-plugin-postcss',
+    "gatsby-plugin-postcss",
 
     // Gallery
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images/`,
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `src`,
+    //     path: `${__dirname}/src/`,
+    //   },
+    // },
   ],
 }
